7d78f3173a99cc0dcd27d624182b39a4147f5790
Krest01 <jakubnagadowski123@gmail.com>
2023-03-02 23:04:22 +0100

    Base for work app

--- main.py
+++ main.py
@@ -1,7 +1,47 @@
-import oauth2
 import requests
-import json
-auth_key = requests.get('https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=dold7hl7sxyqktqr5e17iq605p83ec')
-response = requests.get('https://www.twitch.tv/xntentacion/clips?filter=clips&range=24hr', auth=auth_key)
+from datetime import date, timedelta
+import os
+from moviepy.editor import VideoFileClip
+
+
+yesterday = date.today() - timedelta(1)
+yesterday = str(yesterday)
+yesterday_start = yesterday+'T00:00:00Z'
+yesterday_end = yesterday+'T23:59:59Z'
+
+
+client_id = '37zxvqwg12hts6aji5ucmd34h36pww'
+client_secret = 'afw6htfiii1ipgogjuo722f2pln6o5'
+streamer_name = 'xntentacion'
+
+
+body = {
+    'client_id': client_id,
+    'client_secret': client_secret,
+    "grant_type": 'client_credentials'
+}
+
+
+response = requests.post('https://id.twitch.tv/oauth2/token', body)
+auth_token = response.json()
+
+
+headers = {
+    'Client-ID': client_id,
+    'Authorization': 'Bearer ' + auth_token['access_token'],
+}
+
+
+clips = requests.get(f'https://api.twitch.tv/helix/clips?broadcaster_id=461774409&first=20&started_at={yesterday_start}&ended_at{yesterday_end}', headers=headers)
+clips_data = clips.json()
+
+
+title = clips_data['data'][0]['title']
+clip_to_download = []
+for clip in clips_data['data']:
+    clip_to_download.append(clip['url'])
+
+for link in clip_to_download:
+    os.system(f'twitch-dl download -q 1080p {link}')
 
 
